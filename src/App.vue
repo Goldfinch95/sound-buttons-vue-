2a3015c35974b83ce27314dc0705a7a73338d5a5
Goldfinch <facu_vila@hotmail.com>
2023-07-10 13:37:50 -0300

    buttons with sounds

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<!--Caja Padre: aqui se encuentra los diversos botones. debes poner, en este caso, de donde sacas los datos y la funcion-->
+
+<script>
+import Button from "./components/Button.vue";
+export default {
+  data() {
+    return {
+      buttons: [
+        {
+          name: "pato",
+          img: "src/assets/images/duck.png",
+          sound: "src/assets/sounds/quack.mp3",
+        },
+        {
+          name: "corneta",
+          img: "src/assets/images/airhorn.png",
+          sound: "src/assets/sounds/airhorn.mp3",
+        },
+        {
+          name: "grillo",
+          img: "src/assets/images/cricket.png",
+          sound: "src/assets/sounds/cricket.mp3",
+        },
+        {
+          name: "aplausos",
+          img: "src/assets/images/clap.png",
+          sound: "src/assets/sounds/golf_clap.mp3",
+        },
+        {
+          name: "trompeta",
+          img: "src/assets/images/trumpet.png",
+          sound: "src/assets/sounds/sad_horn.mp3",
+        },
+        {
+          name: "bateria",
+          img: "src/assets/images/batery.png",
+          sound: "src/assets/sounds/ba_dum_tsss.mp3",
+        },
+      ],
+    };
+  },
+  methods: {
+    playAudio(button) {
+      const audio = new Audio(button.sound);
+      audio.play();
+    },
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<template>
+  <div class="box_buttons">
+    <div class="button_container">
+      <Button
+        v-for="button in buttons"
+        :key="button.name"
+        @click="playAudio(button)"
+        :button="button"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.box_buttons {
+  display: flex;
+  height: auto;
+  width: 100%;
+  padding: 15px;
+  border: none;
+  background-color: #111214;
+  border-radius: 12px;
+  color: #fff;
+}
+.button_container {
+  width: 80vw;
+  height: 60vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,56 @@
+<!--Caja hijo, aqui solo involucra la propiedad del hijo que proviene de la caja padre-->
+
+<script>
+export default {
+  props: {
+    button: {
+      name: {
+        type: String,
+        required: true,
+      },
+      img: {
+        type: String,
+        required: true,
+      },
+      sound: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+};
+</script>
+
+<template>
+  <button>
+    <img :src="button.img" />
+    {{ button.name }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  display: flex;
+  justify-content: space-around;
+  margin: 25px;
+  margin-top: 60px;
+  padding: 13px;
+  background-color: #2b2d31;
+  height: 5vh;
+  width: 27vw;
+  border-radius: 7px;
+  font-size: 13px;
+  text-align: end;
+  border: none;
+  color: #fff;
+}
+button:focus {
+  border: 1px solid green;
+}
+
+img {
+  height: 100%;
+}
+</style>
+
+<!--import jsonData from "../assets/data.json"; console.log(jsonData);-->
